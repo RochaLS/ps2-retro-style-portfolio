01ca1146b73227092ab991cb7081e61d287b27ed
Lucas Rocha <ld.rocha@hotmail.com>
2025-11-12 15:33:40 -0800

    ios fullscreen fix

--- src/components/CRT.tsx
+++ src/components/CRT.tsx
@@ -6,5 +6,5 @@ interface CRTProps {
   }
 
 export default function CRT({ children }: CRTProps) {
-  return <div className="crt relative overflow-hidden">{children}</div>;
+  return <div className="crt relative overflow-hidden min-h-screen">{children}</div>;
 }
